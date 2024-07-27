493d4ae2c2d276517015aaf842c2f2cf861dcf33
sergio-tr <truebanosergio@gmail.com>
2024-07-27 19:06:18 +0200

    exercism rust exercise 3

---
+++ gigasecond/src/lib.rs
@@ -0,0 +1,7 @@
+use time::{Duration, PrimitiveDateTime as DateTime};
+const GIGA : i64 = 1_000_000_000;
+
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    start + Duration::seconds(GIGA)
+}
